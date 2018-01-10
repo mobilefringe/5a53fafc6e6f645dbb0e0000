243ac2ea82c72a4a66a723d1288e4049839bee48
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-10 16:38:55 -0500

    St. Vital 2.0 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -208,7 +208,7 @@ function store_search() {
         $("#search_results_stores").hide();
         $('#site_search').val('');
         $('#search_icon').show();
-        $("#site_search").hide();
+        $("#close_search").hide();
     })
     $('#site_search').keyup(function(){
         if ($('#site_search').val() == ""){
