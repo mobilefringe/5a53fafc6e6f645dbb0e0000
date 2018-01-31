879a50618c3b86bad623c1afb314eb11533dabe2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-31 18:07:38 -0500

    St. Vital 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -156,7 +156,7 @@ function renderHomeHours(container, template, collection){
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = moment(val.open_time).tz(getPropertyTimeZone());
             var close_time = moment(val.close_time).tz(getPropertyTimeZone());
-            val.open = "Open Today";
+            val.open = "Today's Hours :";
             val.hours = open_time.format("h a") + " - " + close_time.format("h a");
             val.open_now = "Open Now"
             val.open_color = "#6dd304"
