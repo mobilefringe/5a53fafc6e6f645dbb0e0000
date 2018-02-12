e1b6b6edf6b4aded9fd790e9bacac0910f628291
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-12 12:10:43 -0500

    St. Vital 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -110,6 +110,19 @@ function renderEventDetails(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
+function renderSingleItem(container, template, val){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    // $.each( collection , function( key, val ) {
+        console.log("renderGeneral",val)
+        var repo_rendered = Mustache.render(template_html,val);
+        item_rendered.push(repo_rendered);
+    // });
+    $(container).html(item_rendered.join(''));
+}
+
 function renderGeneral(container, template, collection){
     var item_list = [];
     var item_rendered = [];
